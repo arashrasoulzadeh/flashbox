3b83f5d51f7d8216c268c9a030bfcf83548c7450
arashrasoulzadeh <arashrasoulzadeh@gmail.com>
2021-09-01 03:19:39 +0430

    admin user seed

---
+++ database/seeders/UserAdminSeed.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Role;
+use App\Models\User;
+use App\Models\UserRole;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class UserAdminSeed extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make("12345678")
+        ]);
+        UserRole::create([
+            'user_id' => $user->id,
+            'role_id' => Role::ADMIN_ROLE_ID
+        ]);
+    }
+}
